5c92644841402921e820f081cf68f83bfb9603a0
Sébastien Gourmet <sebastien.gourmet@student.hepl.be>
2025-03-13 10:42:31 +0100

    Fin test MP3

--- ModuleOnly/Module MP3/test/test.ino
+++ ModuleOnly/Module MP3/test/test.ino
@@ -1,8 +1,22 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
 #include <JQ6500_Serial.h>
 
-JQ6500_Serial mp3(serial)
+SoftwareSerial mySerial(17, 16); //RX,TX
+JQ6500_Serial mp3(mySerial);
 
-void setup(){
-  Serial.begin(9600)
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
   mp3.reset();
+  mp3.setVolume(8);
+  mp3.setLoopMode(MP3_LOOP_NONE); 
+}
+
+void loop() {
+  mp3.playFileByIndexNumber(2);
+  delay(1000);
+  mp3.playFileByIndexNumber(1);
+  delay(1000);
+
 }
